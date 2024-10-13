77b56df4c5593ca7bec60cc2b8e490ac4f4fb937
Wise Muronha <wisenielsmuronha@gmail.com>
2024-10-13 21:58:34 +0200

    nothing

--- sessao4/aula3_UUID.py
+++ sessao4/aula3_UUID.py
@@ -1,4 +1,13 @@
 from uuid import uuid4
 from pydantic import BaseModel, Field, UUID4, ValidationError
 
-print(uuid4())
+# print(uuid4())
+
+
+class Person(BaseModel):
+    id: UUID4
+
+
+p = Person(id=uuid4())
+print(p)
+print(p.model_dump_json())
